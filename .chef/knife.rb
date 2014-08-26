8404673e3bd22c6a9721bcfaa9260f8ec75c32c0
Koji Tanaka <kj.tanaka@gmail.com>
2014-08-25 23:56:06 -0400

    update knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,7 +3,24 @@ node_path        "nodes"
 role_path        "roles"
 environment_path "environments"
 data_bag_path    "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
 ssl_verify_mode  :verify_peer
+
+#
+# Setting for Encrypted Data Bag
+#
+#encrypted_data_bag_secret "data_bag_key"
+
+#
+# Example of Chef Server Setting
+#
+#chef_repo = Dir.pwd
+#log_level                :info
+#log_location             STDOUT
+#node_name                'root'
+#client_key               '#{chef_repo}/.chef/user.pem'
+#validation_client_name   'chef-validator'
+#validation_key           '/etc/chef-server/chef-validator.pem'
+#chef_server_url          'https://chef.example.com:443'
+#syntax_check_cache_path  '#{chef_repo}/.chef/syntax_check_cache'
--- shell/prepare_chef.sh
+++ shell/prepare_chef.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 
 sudo apt-get -y install curl
-\curl -sSL https://get.rvm.io | bash
+echo 'export rvm_prefix="$HOME"' > /root/.rvmrc
+echo 'export rvm_path="$HOME/.rvm"' >> /root/.rvmrc
+\curl -sSL https://get.rvm.io | bash -s stable
 source $HOME/.rvm/scripts/rvm
 rvm install 2.1.1
 rvm use 2.1.1 --default
