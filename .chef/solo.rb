46bb2e6e6939894f3384e2b61ca41ef21290020d
Koji Tanaka <kj.tanaka@gmail.com>
2014-09-03 22:26:12 -0400

    updating solo.rb

--- .chef/solo.rb
+++ .chef/solo.rb
@@ -1,12 +1,12 @@
 chef_repo = Dir.pwd
 cookbook_path    ["#{chef_repo}/cookbooks", "#{chef_repo}/site-cookbooks"]
-node_path        "nodes"
-role_path        "roles"
-environment_path "environments"
-data_bag_path    "data_bags"
+node_path        "#{chef_repo}/nodes"
+role_path        "#{chef_repo}/roles"
+environment_path "#{chef_repo}/environments"
+data_bag_path    "#{chef_repo}/data_bags"
 file_cache_path "#{chef_repo}/.cache"
 file_backup_path "#{chef_repo}/.backup"
 ssl_verify_mode :verify_peer
-#encrypted_data_bag_secret "data_bag_key"
+#encrypted_data_bag_secret "#{chef_repo}/data_bag_key"
 #
-#knife[:berkshelf_path] = "cookbooks"
+#knife[:berkshelf_path] = "#{chef_repo}/cookbooks"
